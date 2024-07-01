243bc55083bec94472b7afe1a102a95e209dc591
moonway <59399356+moonway-dev@users.noreply.github.com>
2024-07-01 06:09:02 +0300

    Update auth-context.tsx

--- components/context/auth-context.tsx
+++ components/context/auth-context.tsx
@@ -18,15 +18,13 @@ interface AuthProviderProps {
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-const PROXY_URL = 'https://corsproxy.io/?';
-
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
-      fetch(`${PROXY_URL}https://login.xsolla.com/api/users/me`, {
+      fetch('https://api.screwltd.com/v3/xsolla/user', {
         headers: { Authorization: token },
       })
         .then(response => {
@@ -50,31 +48,39 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const login = async (email: string, password: string, rememberMe: boolean) => {
     try {
       const response = await fetch(
-        `${PROXY_URL}https://login.xsolla.com/api/login?projectId=41fc3f33-4047-44a9-8868-476848f9d438`,
+        'https://api.screwltd.com/v3/xsolla/login',
         {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ username: email, password: password }),
+          body: JSON.stringify({ email: email, password: password }),
         }
       );
+  
       if (!response.ok) {
         throw new Error('Login failed');
       }
+  
       const data = await response.json();
+  
       const token = new URL(data.login_url).searchParams.get('token');
+  
       if (token) {
         if (rememberMe) {
           localStorage.setItem('token', token);
         }
-        const userResponse = await fetch(`${PROXY_URL}https://login.xsolla.com/api/users/me`, {
+  
+        const userResponse = await fetch('https://api.screwltd.com/v3/xsolla/user', {
           headers: { Authorization: token },
         });
+
         if (!userResponse.ok) {
           throw new Error('Failed to fetch user');
         }
+  
         const userData = await userResponse.json();
+  
         setUser(userData);
       } else {
         throw new Error('Token not found in the response');
@@ -84,6 +90,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
       throw error;
     }
   };
+  
 
   const logout = () => {
     localStorage.removeItem('token');
